99dfbb524826cb1761c15770d23e8f7022d9a59e
Yipeng Sun <syp@umd.edu>
2018-05-29 05:09:45 -0400

    Reorganize.

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b master
-  git add . *.html
+  git add index.html
   git add assets
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
