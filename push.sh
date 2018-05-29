7ac5781f7bc7ee0da176343913e39db07c57efc3
Yipeng Sun <syp@umd.edu>
2018-05-29 05:41:16 -0400

    Change push settings.

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com/umd-lhcb/umd-lhcb.github.io.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages master
 }
 
