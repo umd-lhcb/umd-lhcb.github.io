9bc2f3e06ba8d827a0526e5019df5b92d16b7298
Yipeng Sun <syp@umd.edu>
2018-10-31 18:44:59 -0400

    Add comment so we know why we need it.

--- _plugins/hook-post_render.rb
+++ _plugins/hook-post_render.rb
@@ -1,4 +1,5 @@
-Jekyll::Hooks.register :pages, :post_render do |post|
+# Replace escaped html tags so 'doi' links are correctly displayed
+Jekyll::Hooks.register(:pages, :post_render) do |post|
   post.output = post.output.gsub('&lt;', '<')
   post.output = post.output.gsub('&gt;', '>')
 end
